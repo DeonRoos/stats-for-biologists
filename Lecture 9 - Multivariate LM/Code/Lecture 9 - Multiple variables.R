d31c0d1cbe1b6f6fb70c7dc940807009b837c4cb
Deon88 <deonroos99@hotmail.com>
2024-07-26 13:10:23 +0100

    Wrapped up draft of 1st multivaraibles lecture

--- Lecture 9 - Multivariate LM/Code/Lecture 9 - Multiple variables.R
+++ Lecture 9 - Multivariate LM/Code/Lecture 9 - Multiple variables.R
@@ -116,3 +116,242 @@ p <- ggplot(nu_data) +
   sbs_theme()
 
 ggsave(here("Lecture 9 - Multivariate LM/Figures", file = "beaver_wolf.png"), plot = p, width = 650/72, height = 775/72, dpi = 72)
+
+
+
+# Model fitting -----------------------------------------------------------
+
+df1 <- data.frame(
+  b0 = c(8, 0, 6, 3, 4, 3.4),  # intercept
+  b1 = c(-7, 4, -2, 5, 3, 2.4),  # slope for wolf (Present vs Absent)
+  b2 = c(0.5, 1.5, -1, 0, 1, 1.6),  # slope for survival
+  grp = factor(1:6)
+)
+
+combined_df <- bind_rows(
+  lapply(1:nrow(df1), function(i) {
+    temp_df <- yst_dat
+    temp_df$pred <- df1$b0[i] + df1$b1[i] * temp_df$present + df1$b2[i] * temp_df$survival
+    temp_df$grp <- df1$grp[i]
+    temp_df$fit_mean <- df1$b0[i] + df1$b1[i] * temp_df$present + df1$b2[i] * temp_df$survival
+    temp_df$b0 <- df1$b0[i]
+    temp_df$b1 <- df1$b1[i]
+    temp_df$b2 <- df1$b2[i]
+    temp_df
+  })
+)
+
+a1 <- ggplot(combined_df, aes(x = survival, y = beaver, group = grp)) +
+  geom_point(size = 2, aes(color = wolf)) +
+  geom_abline(data = df1, aes(intercept = b0 + b1, slope = b2), 
+              size = 1.5, show.legend = FALSE, colour = "red") +
+  geom_abline(data = df1, aes(intercept = b0, slope = b2), 
+              size = 1.5, show.legend = FALSE, colour = "orange") +
+  geom_segment(aes(x = survival, xend = survival, y = beaver, yend = fit_mean), 
+               linetype = "dashed", color = "white", alpha = 0.5) +
+  scale_color_manual(values = c("Present" = "red", "Absent" = "orange")) +
+  labs(x = "Survival",
+       y = "Beaver Density",
+       color = "Wolf Presence",
+       subtitle = '{paste("mu =", round(df1[df1$grp == closest_state, "b0"], 2), 
+                          "+", round(df1[df1$grp == closest_state, "b1"], 2), "* W",
+                          "+", round(df1[df1$grp == closest_state, "b2"], 2), "* S")}') +
+  sbs_theme() +
+  transition_states(grp) + 
+  ease_aes("cubic-in-out")
+
+anim_save(here("Lecture 9 - Multivariate LM/Figures", file = "fitting.gif"), animation = a1)
+
+
+# Calculate densities for each group
+density_df <- bind_rows(
+  lapply(unique(combined_df$grp), function(g) {
+    temp_df <- combined_df %>% filter(grp == g)
+    x_vals <- seq(min(temp_df$beaver) - 10, max(temp_df$beaver) + 10, length.out = 75)
+    densities <- dnorm(x_vals, mean = mean(temp_df$fit_mean), sd = 1.69)
+    data.frame(x = x_vals, y = densities, grp = g)
+  })
+)
+
+# Update df1 with the new model parameters
+df1 <- df1 %>%
+  mutate(subtitle_text = paste0("beta[0] == ", round(b0, 2), ", ", "beta[1] == ", round(b1, 2), ", ", "beta[2] == ", round(b2, 2)))
+
+# Join the new df1 to combined_df
+combined_df <- combined_df %>%
+  left_join(df1, by = "grp")
+
+# Create the histogram with likelihood overlay plot
+a2 <- ggplot() +
+  geom_histogram(data = combined_df, aes(x = beaver, y = ..density..), binwidth = 1, alpha = 0.5, position = "identity") +
+  geom_line(data = density_df, aes(x = x, y = y), size = 1.5) +
+  scale_x_continuous(limits = c(min(combined_df$beaver) - 2, max(combined_df$beaver) + 2)) +
+  labs(x = "Beaver Density", 
+       y = "Density",
+       fill = "Wolf Presence",
+       subtitle = '{paste("mu =", round(df1[df1$grp == closest_state, "b0"], 2), "+ ", 
+       round(df1[df1$grp == closest_state, "b1"], 2), "* W", "+ ", 
+       round(df1[df1$grp == closest_state, "b2"], 2), "* S")}') +
+  sbs_theme() +
+  transition_states(grp) + 
+  ease_aes("cubic-in-out")
+a2
+
+anim_save(here("Lecture 9 - Multivariate LM/Figures", file = "data_v_dnorm.gif"), animation = a2)
+
+
+sigma_values <- c(10, 6, 0.9, 4, 2, 1.69)
+
+# Calculate densities for each group with varying sigma
+density_df <- bind_rows(
+  lapply(unique(combined_df$grp), function(g) {
+    temp_df <- combined_df %>% filter(grp == g)
+    x_vals <- seq(min(temp_df$beaver) - 2, max(temp_df$beaver) + 2, length.out = 75)
+    sigma <- sigma_values[as.numeric(g)]
+    densities <- dnorm(x_vals, mean = mean(temp_df$fit_mean), sd = sigma)
+    data.frame(x = x_vals, y = densities, grp = g, sigma = sigma)
+  })
+)
+df1 <- df1 %>%
+  mutate(subtitle_text = paste0("beta[0] == ", round(b0, 2), ", ", 
+                                "beta[1] == ", round(b1, 2), ", ", 
+                                "beta[2] == ", round(b2, 2)),
+         sigma = sigma_values)
+combined_df <- combined_df %>%
+  left_join(df1, by = "grp")
+
+a3 <- ggplot() +
+  geom_histogram(data = combined_df, aes(x = beaver, y = ..density..), binwidth = 1, alpha = 0.5, position = "identity") +
+  geom_line(data = density_df, aes(x = x, y = y), size = 1.5) +
+  scale_x_continuous(limits = c(min(combined_df$beaver) - 2, max(combined_df$beaver) + 2)) +
+  labs(
+    x = "Beaver Density", 
+    y = "Density",
+    fill = "Wolf Presence",
+    subtitle = '{paste("mu =", round(df1[df1$grp == closest_state, "b0"], 2), 
+                     "+", round(df1[df1$grp == closest_state, "b1"], 2), "* W",
+                     "+", round(df1[df1$grp == closest_state, "b2"], 2), "* S", 
+                     "\nsigma =", round(df1[df1$grp == closest_state, "sigma"], 2))}'
+  ) +
+  sbs_theme() +
+  transition_states(grp) + 
+  ease_aes("cubic-in-out")
+
+anim_save(here("Lecture 9 - Multivariate LM/Figures", file = "data_v_dnorm_sd.gif"), animation = a3)
+
+m2 <- lm(beaver ~ wolf + survival, data = yst_dat)
+
+nu_data <- expand.grid(wolf = unique(yst_dat$wolf),
+                       survival = seq(0,1,length.out = 10
+                                      ))
+prds <- predict(m2, nu_data, se.fit = TRUE)
+nu_data$fit <- prds$fit
+nu_data$low <- prds$fit - prds$se.fit * 1.96
+nu_data$upp <- prds$fit + prds$se.fit * 1.96
+
+p <- ggplot(nu_data) +
+  geom_line(aes(x = survival, y = fit, colour = wolf)) +
+  geom_ribbon(aes(x = survival, ymin = low, ymax = upp, fill = wolf), alpha = 0.5) +
+  scale_colour_brewer(palette = "Dark2") +
+  scale_fill_brewer(palette = "Dark2") +
+  scale_y_continuous(limits = c(0, NA)) +
+  scale_x_continuous(limits = c(0, 1)) +
+  labs(x = "Beaver survival",
+       colour = "Wolves",
+       fill = "Wolves",
+       y = "Beaver density") +
+  sbs_theme()
+p
+ggsave(here("Lecture 9 - Multivariate LM/Figures", file = "beaver_wolf_survival.png"), plot = p, width = 650/72, height = 775/72, dpi = 72)
+
+
+
+# Complex model -----------------------------------------------------------
+
+m3 <- lm(beaver ~ wolf + survival + reproduction + park, data = yst_dat)
+
+nu_data <- expand.grid(wolf = "Present",
+                      survival = seq(min(yst_dat$survival), max(yst_dat$survival), 0.1),
+                      reproduction = median(yst_dat$reproduction),
+                      park = "Fully protected")
+prds <- predict(m3, nu_data, se.fit = TRUE)
+nu_data$fit <- prds$fit
+nu_data$low <- prds$fit - prds$se.fit * 1.96
+nu_data$upp <- prds$fit + prds$se.fit * 1.96
+
+p1 <- ggplot(nu_data) +
+  geom_ribbon(aes(x = survival, ymin = low, ymax = upp), alpha = 0.3, fill = "lightgrey") +
+  geom_line(aes(x = survival, y = fit)) +
+  scale_colour_brewer(palette = "Dark2") +
+  scale_fill_brewer(palette = "Dark2") +
+  labs(x = "Beaver survival",
+       colour = "Wolves",
+       fill = "Wolves",
+       y = "Beaver density") +
+  sbs_theme()
+p1
+
+nu_data <- expand.grid(wolf = c("Present", "Absent"),
+                       survival = median(yst_dat$survival),
+                       reproduction = median(yst_dat$reproduction),
+                       park = "Fully protected")
+nu_data$wolf <- factor(nu_data$wolf, levels = c("Absent", "Present"))
+prds <- predict(m3, nu_data, se.fit = TRUE)
+nu_data$fit <- prds$fit
+nu_data$low <- prds$fit - prds$se.fit * 1.96
+nu_data$upp <- prds$fit + prds$se.fit * 1.96
+
+p2 <- ggplot(nu_data) +
+  geom_point(aes(x = wolf, y = fit)) +
+  geom_errorbar(aes(x = wolf, ymin = low, ymax = upp), width = 0.1, colour = "white") +
+  labs(x = "Wolf presence",
+       y = "Beaver density") +
+  sbs_theme()
+p2
+
+nu_data <- expand.grid(wolf = "Present",
+                       survival = median(yst_dat$survival),
+                       reproduction = seq(min(yst_dat$reproduction), max(yst_dat$reproduction), 0.25),
+                       park = "Fully protected")
+prds <- predict(m3, nu_data, se.fit = TRUE)
+nu_data$fit <- prds$fit
+nu_data$low <- prds$fit - prds$se.fit * 1.96
+nu_data$upp <- prds$fit + prds$se.fit * 1.96
+
+p3 <- ggplot(nu_data) +
+  geom_ribbon(aes(x = reproduction, ymin = low, ymax = upp), alpha = 0.3, fill = "lightgrey") +
+  geom_line(aes(x = reproduction, y = fit)) +
+  scale_colour_brewer(palette = "Dark2") +
+  scale_fill_brewer(palette = "Dark2") +
+  labs(x = "Beaver reproduction",
+       colour = "Wolves",
+       fill = "Wolves",
+       y = "Beaver density") +
+  sbs_theme()
+p3
+
+nu_data <- expand.grid(wolf = "Present",
+                       survival = median(yst_dat$survival),
+                       reproduction = median(yst_dat$reproduction),
+                       park = c("Fully protected", "Partially protected"))
+prds <- predict(m3, nu_data, se.fit = TRUE)
+nu_data$fit <- prds$fit
+nu_data$low <- prds$fit - prds$se.fit * 1.96
+nu_data$upp <- prds$fit + prds$se.fit * 1.96
+
+p4 <- ggplot(nu_data) +
+  geom_point(aes(x = park, y = fit)) +
+  geom_errorbar(aes(x = park, ymin = low, ymax = upp), width = 0.1, colour = "white") +
+  labs(x = "Park protection",
+       y = "Beaver density") +
+  sbs_theme()
+p4
+
+design <- "
+AB
+CD
+"
+
+p <- p1 + p4 + p3 + p2 + plot_layout(design = design)
+ggsave(here("Lecture 9 - Multivariate LM/Figures", file = "complex_results.png"), plot = p,width = 1400/72, height = 650/72, dpi = 72)
+ggsave(here("Lecture 9 - Multivariate LM/Figures", file = "beaver_no_wolf.png"), plot = p2, width = 650/72, height = 775/72, dpi = 72)
